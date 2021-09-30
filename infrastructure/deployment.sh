e6298f8251a7f72bf8cb7042aa94ad80939be584
lenoxy <lenoxy@gmx.ch>
2021-09-30 15:28:35 +0200

    add deployment script to git

---
+++ infrastructure/deployment.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo Deployment Script
+
+cd /root/git/mock-stock || exit
+
+# pull release
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$BRANCH" != "master" ]]; then
+  echo 'Aborting script, wrong branch';
+  echo "$BRANCH";
+  exit 1;
+fi
+
+# clear any changes (possibly package-lock.json)
+git reset --hard
+
+git pull origin master
+
+docker-compose down
+
+cd ./backend
+pip install -r requirements.txt
+cd ..
+
+cd ./frontend
+npm install
+npm run build
+cd ..
+
+docker-compose up -d --force-recreate --build
