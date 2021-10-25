42617c8bbd5b2e828078d5b3e8228d9f2db06c6a
Cedric Gasser <cedigasser@gmail.com>
2021-10-25 07:18:59 +0200

    Add login_required to sell/buy

--- backend/src/controller/stocks.py
+++ backend/src/controller/stocks.py
@@ -1,4 +1,4 @@
-import flask_login
+from flask_login import login_required, current_user
 from flask import Blueprint, request, jsonify
 import yfinance as yf
 import finance
@@ -21,22 +21,21 @@ def get_stock(id):
         return str(e), 400
 
 
-
 @stocks.route("/stocks/<string:id>/buy", methods=['PUT'])
-# @login_required
+@login_required
 def buy_stock(id):
     amount = request.json['amount']
 
-    db.update_owned_stocks(OwnedStock({'username': flask_login.current_user.username,'id': id, 'amount': amount }))
+    db.update_owned_stocks(OwnedStock({'username': current_user.username,'id': id, 'amount': amount }))
 
-    return db.get_owned_stocks(flask_login.current_user.username)[id].to_json()
+    return db.get_owned_stocks(current_user.username)[id].to_json()
 
 
 @stocks.route("/stocks/<string:id>/sell", methods=['PUT'])
-# @login_required
+@login_required
 def sell_stock(id):
     amount = -(request.json['amount'])
 
-    db.update_owned_stocks(OwnedStock({'username': flask_login.current_user.username,'id': id, 'amount': amount }))
+    db.update_owned_stocks(OwnedStock({'username': current_user.username,'id': id, 'amount': amount }))
 
-    return db.get_owned_stocks(flask_login.current_user.username)[id].to_json()
+    return db.get_owned_stocks(current_user.username)[id].to_json()
