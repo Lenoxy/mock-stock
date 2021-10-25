ba61b1fbc7cb646fbb9e1f358b8a4152a0d267dc
daniel <danypolgar@gmail.com>
2021-10-25 19:36:37 +0200

    basic component templtes added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RegisterComponent} from "./register/register.component";
+import {LeaderboardComponent} from "./leaderboard/leaderboard.component";
+import {LoginComponent} from "./login/login.component";
+import {ProfileComponent} from "./profile/profile.component";
+import {StockListComponent} from "./stock-list/stock-list.component";
+import {StockDetailComponent} from "./stock-detail/stock-detail.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'user/:id', component: ProfileComponent },
+  { path: 'stock-list', component: StockListComponent },
+  { path: 'stock-detail', component: StockDetailComponent },
+  { path: 'leaderboard', component: LeaderboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {ButtonModule} from "primeng/button";
 import {AvatarModule} from "primeng/avatar";
+import {RegisterComponent} from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { StockDetailComponent } from './stock-detail/stock-detail.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PurchaseComponent } from './purchase/purchase.component';
+import { SellComponent } from './sell/sell.component';
+import { StockListComponent } from './stock-list/stock-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    LeaderboardComponent,
+    StockDetailComponent,
+    ProfileComponent,
+    PurchaseComponent,
+    SellComponent,
+    StockListComponent
   ],
   imports: [
     BrowserModule,
@@ -21,4 +37,5 @@ import {AvatarModule} from "primeng/avatar";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
