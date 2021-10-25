12805f99d3b5e812b9797510a765bbc3bd54ffa2
Cedric Gasser <cedigasser@gmail.com>
2021-10-25 18:49:32 +0200

    Refactor stocks and add Errorhandling

--- backend/src/models/Stock.py
+++ backend/src/models/Stock.py
@@ -1,21 +1,17 @@
 class Stock():
     def __init__(self) -> None:
-        self.id: str
-        self.value: float
+        self.id: str = None
+        self.value: float = None
         self.history: dict = None
-        self.name: str
-        self.change: float
+        self.name: str = None
+        self.change: float = None
+        self.amount: int = None
 
 
     def to_json(self) -> dict:
-        if self.history:
-            return {'id': self.id,
-                'value': self.value,
-                'name': self.name,
-                'change': self.change,
-                'history': self.history,}
-
         return {'id': self.id,
-                'value': self.value,
-                'name': self.name,
-                'change': self.change,}
+            'value': self.value,
+            'name': self.name,
+            'change': self.change,
+            'history': self.history,
+            'amount': self.amount,}
