0f1328a900669b61c8f25f6321570db6f5f1a907
Philip Baumann <philip.baumann@hispeed.ch>
2021-11-04 17:05:25 +0100

    Fixes in Deployment script

--- infrastructure/deployment.sh
+++ infrastructure/deployment.sh
@@ -4,34 +4,54 @@ echo Deployment Script
 cd /home/mock-stock/mock-stock || exit
 
 # pull release
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-if [[ "$BRANCH" != "master" ]]; then
-  echo 'Aborting script, wrong branch';
-  echo "$BRANCH";
-  exit 1;
-fi
+#BRANCH=$(git rev-parse --abbrev-ref HEAD)
+#if [[ "$BRANCH" != "master" ]]; then
+#  echo 'Aborting script, wrong branch';
+#  echo "$BRANCH";
+#  exit 1;
+#fi
 
-# clear any changes (possibly package-lock.json)
-git reset --hard
+docker-compose --env-file ./docker-compose.env down
 
-echo grep "VERSION=" docker-compose.env | cut -d '=' -f 2
-
-
-$VERSION=$(grep "VERSION=" docker-compose.env | cut -d '=' -f 2)
-
-echo "$VERSION"
-
-git pull origin master
-
-docker-compose down
-
-cd ./backend
-pip install -r requirements.txt
-cd ..
-
-cd ./frontend
-npm install
-npm run build
-cd ..
+#git reset --hard
+#git pull origin master
 
 docker-compose -d --force-recreate --build --env-file ./docker-compose.env up
+
+CURRENT_VERSION="$(grep "VERSION=" ./../docker-compose.env | cut -d '=' -f 2)"
+NEXT_VERSION="$(($CURRENT_VERSION + 1))"
+
+echo $CURRENT_VERSION
+echo $NEXT_VERSION
+
+BACKEND_CONTAINER="$(docker ps -q -f name="mock-stock-backend-1" -f status="running")"
+FRONTEND_CONTAINER="$(docker ps -q -f name="mock-stock-frontend-1" -f status="running")"
+
+if [ -z "$BACKEND_CONTAINER" ] || [ -z "$FRONTEND_CONTAINER"];
+then
+    echo "[BACKEND] shutting down"
+    docker stop mock-stock-backend-$CURRENT_VERSION
+    docker rm -f mock-stock-backend-$CURRENT_VERSION
+    echo "[BACKEND] offline"
+
+    echo "[FRONTEND] shutting down"
+    docker stop mock-stock-frontend-$CURRENT_VERSION
+    docker rm -f mock-stock-frontend-$CURRENT_VERSION
+    echo "[FRONTEND] offline"
+
+    docker-compose -d --force-recreate --build --env-file ./docker-compose.env up
+
+else
+    echo "[BACKEND] removing old image"
+    docker rm -f mock-stock-backend-$PREVIOUS_VERSION
+    docker image rm -f mock-stock-backend-$PREVIOUS_VERSION
+    echo "[Backend] old image is gone"
+
+    echo "[FRONTEND] removing old image"
+    docker rm -f mock-stock-backend-$PREVIOUS_VERSION
+    docker image rm -f mock-stock-backend-$PREVIOSU_VERSION
+    echo "[FRONTEND] old image is gone"
+
+    rm -f docker-compose.env
+    echo VERSION=$NEXT_VERSION  > /home/mock-stock/mock-stock/docker-compose.env
+fi
