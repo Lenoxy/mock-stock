f208a345354a9325c4b65279b0dee3433f79a30c
Philip Baumann <philip.baumann@hispeed.ch>
2021-11-18 15:44:17 +0100

    Deployment script fixed

--- infrastructure/deployment.sh
+++ infrastructure/deployment.sh
@@ -57,6 +57,7 @@ else
     rm -f docker-compose.env
     echo VERSION=$NEXT_VERSION  > /home/mock-stock/mock-stock/docker-compose.env
     echo $NEXT_VERSION
+    git add ./../docker-compose.env
     git commit -m "VERSION UPDATE"
     git push
 
