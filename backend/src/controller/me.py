be974f6f5a12398ec9a37713fddc447f2a8737bf
Cedric Gasser <cedigasser@gmail.com>
2021-12-08 22:50:11 +0100

    Implement /me to use users.get_user()

--- backend/src/controller/me.py
+++ backend/src/controller/me.py
@@ -4,6 +4,7 @@ from flask import Blueprint
 from flask_login import login_required, current_user
 import finance
 import db
+from controller import users
 
 me = Blueprint('me', __name__)
 
@@ -12,23 +13,8 @@ me = Blueprint('me', __name__)
 @login_required
 def get_me():
     try:
-        user = current_user
-        money_in_stocks = 0.0
-
-        owned_stocks = db.get_owned_stocks(current_user.username)
-        stock_ids = [key for _, key in enumerate(owned_stocks)]
-
-        if stock_ids:
-            stocks = finance.get_stocks(stock_ids)
-            for stock in stocks:
-                stock.amount = owned_stocks[stock.id].amount
-                money_in_stocks += stock.amount * stock.value
-
-            user.stocks = [stock.to_dict() for stock in stocks]
-
-        user.money_in_stocks = money_in_stocks
-
-        return user.to_dict()
+        username = current_user.username
+        return users.get_user(username)
 
     except Exception as e:
         return str(e), 400
